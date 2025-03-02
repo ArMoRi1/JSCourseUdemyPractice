3a7eedc1bb710b419878956d01a7b1b81318ebdd
Artem <artemmochalov445@icloud.com>
2025-03-02 13:22:37 -0500

    lesson 12

---
+++ lesson 12/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+// alert("HEllo");
+
+// const result = confirm("are you gay?");
+// console.log("Is he gay? : " + result);
+
+// const answer = prompt("How are old you?", "");
+// console.log(answer);
+
+// const answer = +prompt("How are old you?", "");
+// console.log(answer + 5);//lol
+
+const answers = [];
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your surname?', '');
+answers[2] = prompt('What is your age?', '');
+
+document.write(answers);
