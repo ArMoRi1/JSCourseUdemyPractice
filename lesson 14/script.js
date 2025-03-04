992b60422b87f4a81aeee85c5b6690a19172b921
Artem <artemmochalov445@icloud.com>
2025-03-03 23:49:31 -0500

    lesson 14 оператори

---
+++ lesson 14/script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let increment = 10,
+	decrement = 10;
+
+
+console.log(increment);
+console.log(decrement);
+
+increment++;
+decrement--;
+
+console.log(increment);
+console.log(decrement);
+
+console.log(5%2);
+
+const isChecked = true,
+	  isClosed = false;
+
+console.log(isChecked || isClosed);
+console.log(isChecked && isClosed);
