15cfe4e66bf0c3c751dab3bff4dd73f45387fab2
Artem <artemmochalov445@icloud.com>
2025-03-06 14:11:56 -0500

    lesson 21. while, do-while, for

---
+++ lesson 21/script.js
@@ -0,0 +1,26 @@
+"use strict"
+
+let num = 50;
+
+//while
+
+// while(num <= 55){
+// 	console.log(num);
+// 	num++;
+// }
+
+
+// //do-while
+// do{
+// 	console.log(num);
+// 	num++;
+// } while(num <= 55);
+
+//for
+
+for(let i = 0; i<10; i++){
+	if(i === 6){
+		continue;
+	}
+	console.log(i);
+}
