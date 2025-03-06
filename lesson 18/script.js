7feb280068a6880c2d038008bc39a4370fbba23c
Artem <artemmochalov445@icloud.com>
2025-03-05 23:10:18 -0500

    lesson 18, step 2(practice part1). creating of the personalMovieDB

--- lesson 18/script.js
+++ lesson 18/script.js
@@ -1,3 +1,11 @@
 "use strict"
 const numberOfFilms = prompt("How many films have you watched?", "");
 
+const personalMovieDB = { 
+	count: numberOfFilms, 
+	movies: { }, 
+	actors:{ }, 
+	genres:[], 
+	privat: false, 
+} 
+
