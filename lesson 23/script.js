fbcedca4cfd8b37b795b4f98f5453dc64b627b29
Artem <artemmochalov445@icloud.com>
2025-03-07 00:17:56 -0500

    lesson 23, step 1(practice part2). creating a loop

---
+++ lesson 23/script.js
@@ -0,0 +1,38 @@
+
+
+"use strict"
+const numberOfFilms = +prompt("How many films have you watched?", "");
+
+const personalMovieDB = { 
+	count: numberOfFilms, 
+	movies: { }, 
+	actors:{ }, 
+	genres:[], 
+	privat: false, 
+} 
+
+for(let i=0; i<2; i++){
+	const a = prompt("What was one of the last films that you watched?",""); 
+	const b = prompt("How would you rate this film?",""); 
+	
+	if(	(a!=null) && (b!=null) && (a!='') &&(b!='') && a.length < 50){
+		personalMovieDB.movies[a] =  b;
+		console.log('done');
+	}
+	else{
+		console.log('error');
+		i--;
+	}	
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
