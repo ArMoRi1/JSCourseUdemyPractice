4346d162af1c7aa4dfdd1fc81e1c0408407e69e3
Artem <artemmochalov445@icloud.com>
2025-03-07 16:54:05 -0500

    lessom 27. strings manipulations

---
+++ lesson 27/script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const strng = "tEsT";
+const arr = [1,2,3];
+
+console.log(strng);
+console.log(strng.toUpperCase());
+console.log(strng.toLowerCase());
+
+let apple = "there are a few apples";
+console.log(apple.indexOf("apple"));
+
+
+const logg = "Hello, World!";
+
+console.log(logg.slice(7,13));
+console.log(logg.substring(6,11));
+//console.log(logg.substr()); //this is old stuff
+
+const number = 12.2;
+
+console.log(Math.round(number));
+
+const someText = "12.2px";
+console.log(parseFloat(someText)); 
