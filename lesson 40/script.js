f1cd08fa656f92606bffaf0a47d8cea12b75c806
Artem <artemmochalov445@icloud.com>
2025-03-14 16:00:44 -0400

    lesson40. interview tasks examples

---
+++ lesson 40/script.js
@@ -0,0 +1,26 @@
+"use strict"
+
+// let x = 5; alert( x++ ); answer: 5
+
+// [ ] + false - null + true; answer: NaN
+
+// let y = 1; let x = y = 2; alert(x); answer: 2
+
+// [ ] + 1 + 2; answer: "12"
+
+// alert( "1"[0] ); answer: "1"
+
+// 2 && 1 && null && 0 && undefined; answer: null
+
+// !!( a && b ) === (a && b); answer: false
+
+// alert( null || 2 && 3 || 4 ); answer: 3
+
+// const a = [1, 2, 3], b = [1, 2, 3];
+// a == b  answer: false
+
+// alert( +"Infinity"); answer: Infinity type of number
+
+// "Ёжик" > "яблоко"; answer: false
+
+// 0 || "" || 2 || undefined || true || falsе ; answer: 2
