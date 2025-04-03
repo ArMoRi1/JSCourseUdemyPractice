c5b4ba8f68094cb290273482faa460b95cf177e3
Artem <artemmochalov445@icloud.com>
2025-04-02 23:03:32 -0400

    lesson85. Promices. methods: then(), catch(), finally(), and all(), race()

---
+++ lesson 85/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const test = time =>{
+    return new Promise(resolve =>{
+        setTimeout(()=> resolve(), time);
+    });
+};
+
+test(1000).then(()=>console.log('1000ms'));
+test(2000).then(()=>console.log('2000ms'));
+
+// Promise.all([test(2000), test(1000)]).then(()=>{
+//     console.log('all');
+// });
+
+Promise.race([test(2000), test(1000)]).then(()=>{
+    console.log('all');
+});
+
+
+
+// console.log('data request...');
+//
+// const req = new Promise(function (resolve, reject){
+//     setTimeout(()=>{
+//         console.log('data preparing...');
+//
+//         const product = {
+//             name: 'TV',
+//             price: 2000,
+//         };
+//         resolve(product);
+//
+//     },2000);
+// });
+//
+// req.then((product)=>{
+//
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             product.status = 'order';
+//             resolve(product);
+//             // reject(product);
+//
+//         },2000);
+//     }).then(data =>{
+//         data.modify = true;
+//         return data;
+//     }).then(data =>{
+//         console.log(data);
+//     }).catch(()=>{
+//         console.error('something went wrong!');
+//     }).finally(()=>{
+//         console.log('finally');
+//     });
+// });
+//
+//
+//
