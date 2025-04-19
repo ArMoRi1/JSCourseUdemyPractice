0f91517e8fef90a04224183440e7c4fe93a26c57
Artem <artemmochalov445@icloud.com>
2025-04-19 16:34:47 -0400

    lesson110. some more practice with generators

--- lesson 110/script.js
+++ lesson 110/script.js
@@ -19,14 +19,42 @@
 // console.log(str.next());
 // console.log(str.next());
 
-function* count(n){
-    for(let i=0; i < n; i++){
-        yield i;
-    }
+// function* count(n){
+//     for(let i=0; i < n; i++){
+//         yield i;
+//     }
+// }
+//
+// const counter = count(7);
+//
+// for(let c of count(7)){
+//     console.log(c);
+// }
+
+// function* fibonacci(limit) {
+//     let a = 0, b = 1;
+//     while (a < limit) {
+//         yield a;
+//         [a, b] = [b, a + b];
+//     }
+// }
+// for (let num of fibonacci(100)) {
+//     console.log(num);
+// }
+
+function* gen() {
+    yield 1;
+    yield* ['A', 'B'];
+    yield 2;
+    yield 'z';
+    yield 'z';
 }
 
-const counter = count(7);
+const nnn = gen();
 
-for(let c of count(7)){
-    console.log(c);
+
+for(let k of nnn){
+    console.log(nnn.next().value);
 }
+
+
