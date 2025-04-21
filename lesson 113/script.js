81e9099b01bbd2e64c07b79c776fcb5d6b5d0b0e
Artem <artemmochalov445@icloud.com>
2025-04-21 17:31:15 -0400

    lesson113. order of implementation of default code and async code

---
+++ lesson 113/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+setTimeout(()=>{
+    console.log(1);
+}, 0);
+
+console.log(2);
+
+
+// let k=0;
+// function  count(){
+//     for(let i=0; i<1e9; i++){
+//         k++;
+//     }
+//     alert('done');
+// }
+//
+// count();
+// console.log(1);
+//
+// setTimeout(()=>{
+//     console.log('timeout_2000')
+// }, 2000);
+//
+// setTimeout(()=>{
+//     console.log('timeout_4000')
+// }, 4000);
+//
+// console.log(2);
