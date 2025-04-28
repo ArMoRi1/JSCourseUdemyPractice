30b9b74b3caf6cc576049011981db6bcc7500f78
Artem <artemmochalov445@icloud.com>
2025-04-27 22:21:25 -0400

    lesson125. Created new project. added a startnig HTML structure, CSS using React

---
+++ ReactJS/employees/src/componets/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () =>{
+    return (
+        <div className="app-info">
+            <h1>Company employee accounting N</h1>
+            <h2>Total employees number: {}</h2>
+            <h2>Number of employees who will receive a bonus</h2>
+        </div>
+    );
+}
+
+export default AppInfo;
