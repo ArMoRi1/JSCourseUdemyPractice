76d83f36d738f778b9433e8f941b4ed4ff89bf42
Artem <artemmochalov445@icloud.com>
2025-04-28 20:14:31 -0400

    lesson131.Eventsin React. Remember how to use 'this'

--- ReactJS/lesson-126/src/App.js
+++ ReactJS/lesson-126/src/App.js
@@ -6,8 +6,10 @@ class WhoAmI extends Component{
         super(props);
         this.state = {
             years : 27,
-            text: '+++'
+            text: '+++',
+            position: '',
         }
+        // this.nextYear = this.nextYear.bind(this);
     }
     nextYear = () =>{
         this.setState(state =>({
@@ -15,14 +17,31 @@ class WhoAmI extends Component{
             text: state.text + "+",
         }));
     }
+    // nextYear(){
+    //     this.setState(state =>({
+    //         years: state.years + 1,
+    //         text: state.text + "+",
+    //     }));
+    // }
+    commitInputChanges = (e)=>{
+        // console.log(e.target.value);
+        this.setState({
+            position: e.target.value,
+        });
+    }
 
     render(){
         const {name, surname, link} = this.props;
+        const {position, text, years} = this.state;
         return (
             <div>
-                <button onClick={this.nextYear}>{this.state.text}</button>
-                <h1>My name is {name}, surname - {surname}, age - {this.state.years} </h1>
+                <button onClick={this.nextYear}>{text}</button>
+                <h1>My name is {name}, surname - {surname}, age - {years}, position - {position} </h1>
                 <a href={link}>My profile</a>
+                <form>
+                    <span>Type position:</span>
+                    <input type="text" onChange={this.commitInputChanges}/>
+                </form>
             </div>
         );
     }
