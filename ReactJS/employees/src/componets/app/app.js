a0a142fcc32aad8eff574c1518bfaaf1b64bbb20
Artem <artemmochalov445@icloud.com>
2025-04-30 23:53:39 -0400

    lesson134. Immutability on the event. Made my own event such as onDelete & onAdd

--- ReactJS/employees/src/componets/app/app.js
+++ ReactJS/employees/src/componets/app/app.js
@@ -1,33 +1,68 @@
-import './app.css';
+
+import {Component} from 'react'
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-function App(){
 
-    const data = [
-        {name: 'Art M.', salary: 800  , increase: false, like: false, id: 1},
-        {name: 'Ira H.', salary: 2000 , increase: false, like: true, id: 2},
-        {name: 'Nig A.', salary: 1500 , increase: false, like: false, id: 3},
-    ];
+import './app.css';
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Art M.', salary: 800, increase: false, like: false, id: 1},
+                {name: 'Ira H.', salary: 2000, increase: false, like: true, id: 2},
+                {name: 'Nig A.', salary: 1500, increase: false, like: false, id: 3},
+            ],
+
+        }
+    }
+    deleteItem = (id) =>{
+        this.setState(({data})=>{
+            return {
+                data: data.filter(item => item.id !== id),
+            }
+        });
+    }
+    addItem = (name, salary) =>{
+        const maxId = this.state.data.length > 0 ? Math.max(...this.state.data.map(item => item.id))+1 : 0;
+
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            like: false,
+            id: maxId + 1,
+        }
+        this.setState(({data})=>({
+            data: [...data, newItem]
+        }));
+    }
+    render(){
+        return (
+            <div className="app">
+                <AppInfo/>
 
 
-    return (
-      <div className="app">
-          <AppInfo/>
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
 
-
-          <div className="search-panel">
-              <SearchPanel/>
-              <AppFilter/>
-          </div>
-
-          <EmployeesList data={data}/>
-          <EmployeesAddForm/>
-      </div>
-    );
+                <EmployeesList
+                    data={this.state.data}
+                    onDelete={this.deleteItem}
+                />
+                <EmployeesAddForm
+                    onAdd={this.addItem}
+                />
+            </div>
+        );
+    }
 }
 
 
--- ReactJS/employees/src/componets/employees-add-form/employees-add-form.js
+++ ReactJS/employees/src/componets/employees-add-form/employees-add-form.js
@@ -16,13 +16,27 @@ class EmployeesAddForm extends Component{
            [e.target.name] : e.target.value,
         });
     }
+    onSubmit = (e) =>{
+        e.preventDefault();
+        if(this.state.name.length > 1 && !this.state.salary){
+            return;
+        }
+
+        this.props.onAdd(this.state.name, this.state.salary);
+
+        this.setState({
+            name: '',
+            salary: '',
+        });
+    }
     render(){
         const {name, salary} = this.props;
        return(
            <div className="app-add-form">
                <h3>Add new employee</h3>
                <form
-                   className="add-form d-flex">
+                   className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                    <input type="text"
                           className="form-control new-post-label"
                           placeholder="What is his/her name?"
