d95e6e6660cd5df5a893371385f6a98a9c7f5fa8
Artem <artemmochalov445@icloud.com>
2025-10-16 21:28:27 -0400

    lesson 178. React.memo

--- ReactJS/hooksPractice/src/App.js
+++ ReactJS/hooksPractice/src/App.js
@@ -1,36 +1,84 @@
-import data from './data';
-import {useState, useMemo, useDeferredValue, useTransition} from 'react';
+import {useState, memo, useCallback} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
+
+function propsCompare(prevProps, nextProps){
+    return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text
+}
+
+const Form = memo((props) => {
+        console.log('render');
+        return (
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+                        <input
+                            value={props.mail.name}
+                            onChange={(e) => props.onMailChange(e.target.value)}
+                            type="email"
+                            className='form-control'
+                            id="exampleFormControlInput1"
+                            placeholder="name@example.com"
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea
+                            value={props.text}
+                            onChange={(e) => props.onTextChange(e.target.value)}
+                            className="form-control"
+                            id="exampleFormControlTextarea1"
+                            rows="3"
+                        />
+                    </div>
+                </form>
+            </Container>
+        )
+    }
+    , propsCompare);
 
 function App() {
-    const [text, setText] = useState('');
-    const [posts, setPosts] = useState(data);
-    // const deferredValue = useDeferredValue(text);
-    const [isPending, startTransition] = useTransition();
+    const [data, setData] = useState({
+        mail: {
+            name: 'name@example.com',
+        },
+        text: 'some text'
+    });
 
-    const filteredPosts = useMemo(() => {
-        return posts.filter(item => item.name.toLowerCase().includes(text));
-    }, [text]);
+    const handleMailChange = useCallback((newMail) => {
+        setData(prevData => ({
+            ...prevData,
+            mail: {
+                name: newMail
+            }
+        }));
+    }, []);
 
-    const onValueChange = (e) => {
-        startTransition(()=>{
-            setText(e.target.value)
-        });
-    }
+    const handleTextChange = useCallback((newText) => {
+        setData(prevData => ({
+            ...prevData,
+            text: newText
+        }));
+    }, []);
 
     return (
         <>
-            <input value={text} type='text' onChange={onValueChange}/>
-
-            <hr/>
-
-            <div>
-                {isPending ? <h4>Loading...</h4>:
-                    filteredPosts.map(post => (
-                    <div key={post._id}>
-                        <h4>{post.name}</h4>
-                    </div>
-                ))}
-            </div>
+            <Form
+                mail={data.mail}
+                text={data.text}
+                onMailChange={handleMailChange}
+                onTextChange={handleTextChange}
+            />
+            <button
+                onClick={() => setData({
+                    mail: {
+                        name: 'second@example.com',
+                    },
+                    text: 'another text'
+                })}>
+                Click me
+            </button>
         </>
     );
 }
