70a77cea84acc6af3effea8bbbb2383d5a9ca629
Artem <artemmochalov445@icloud.com>
2025-11-05 13:09:19 -0500

    lesson 195. First usage of React Provider

--- ReactJS/reduxPractice/reduxPracticeProject/src/index.jsx
+++ ReactJS/reduxPractice/reduxPracticeProject/src/index.jsx
@@ -1,50 +1,23 @@
 import React from 'react'
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { legacy_createStore as createStore, bindActionCreators } from 'redux';
-import './index.css'
 import reducer from "./reducer";
-import * as actions from './actions'
+import {Provider} from "react-redux";
+
+import App from "./components/App";
+
 
 const store = createStore(reducer);
 
-const update = () => {
-    document.getElementById('counter').textContent = getState().value;
-}
-
-const {dispatch} = store;
-const {subscribe} = store;
-const {getState} = store;
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) =>(...args)=>{
-//     dispatch(creator(...args));
-// }
-
-const {inc, dec, rnd} = bindActionCreators( actions, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-document.getElementById('rnd').addEventListener('click', ()=>{
-    const value = Math.floor(Math.random() * 10);
-    rnd(value);
-});
-
-
-
-
-
-
-// let state = reducer(initialState, {type: 'INC'});
-//  state = reducer(state, {type: 'INC'});
-//  state = reducer(state, {type: 'INC'});
-//  state = reducer(state, {type: 'INC'});
-// console.log(state);
-
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/*<h1>Hello, Redux!</h1>*/}
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </StrictMode>
 )
+
+
+
