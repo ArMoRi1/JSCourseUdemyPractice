5d7c4f5a7b549865b74874e4b7741bdf70c87460
Artem <artemmochalov445@icloud.com>
2025-11-05 14:11:58 -0500

    lesson 197. Connectinf React&Redux usinnd useSelector & useDispatch

--- ReactJS/reduxPractice/reduxPracticeProject/src/components/Counter.jsx
+++ ReactJS/reduxPractice/reduxPracticeProject/src/components/Counter.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import {connect} from "react-redux";
-import * as actions from '../actions';
-import {bindActionCreators} from "redux";
+import {inc, dec, rnd} from '../actions';
+import {useSelector, useDispatch} from "react-redux";
 
-const Counter = ({counter, inc, dec, rnd}) =>{
+// import {bindActionCreators} from "redux";
+
+const Counter = () =>{
+
+    const counter = useSelector(state => state.counter)
+    const dispatch = useDispatch();
+
     return (
         <div className="jumbotron">
             <h1>{counter}</h1>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
+            <button onClick={()=>{dispatch(dec())}} className="btn btn-primary">DEC</button>
+            <button onClick={()=>{dispatch(inc())}} className="btn btn-primary">INC</button>
+            <button onClick={()=>{dispatch(rnd())}} className="btn btn-primary">RND</button>
         </div>
     )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        counter: state.value,
-    }
-}
+// const mapStateToProps = (state) => {
+//     return {
+//         counter: state.value,
+//     }
+// }
 
 // const mapDispatchToProps = (dispatch) =>{
 //     return bindActionCreators(actions, dispatch)
@@ -26,4 +32,4 @@ const mapStateToProps = (state) => {
 // }
 
 //export default connect(mapStateToProps, mapDispatchToProps)(Counter);
-export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
